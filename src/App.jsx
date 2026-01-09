daaa2ef92bfd2996adb67fe04b03488b5f70288b
lkhyal <lkhyal.ab@gmail.com>
2026-01-09 15:47:47 +0100

    dine with the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="h-dvh bg-black">
+      <div className="h-9/10 flex">
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
